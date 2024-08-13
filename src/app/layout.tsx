a6a259c101fa5b21377fee5d60ee926e60bca2ec
João Guilherme <joaaogui@gmail.com>
2024-08-13 20:29:57 -0300

    Fix buils errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,35 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
- 
 const metadata: Metadata = {
   title: "Lumis",
   description: "Clinica de Estética",
 };
 
+import { Dancing_Script, Montserrat } from "next/font/google";
 
-import { Dancing_Script, Montserrat } from 'next/font/google';
- 
-export const montserrat = Montserrat({
-  subsets: ['latin'],
-  variable: '--font-montserrat',
-})
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
-export const dancingScript = Dancing_Script({
-  subsets: ['latin'],
-  variable: '--font-dancing-script',
-})
+const dancingScript = Dancing_Script({
+  subsets: ["latin"],
+  variable: "--font-dancing-script",
+});
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" >
-
-      <body className={`${dancingScript.variable} ${montserrat.variable} font-body`} suppressHydrationWarning={true}>{children}</body>
+    <html lang="en">
+      <body
+        className={`${dancingScript.variable} ${montserrat.variable} font-body`}
+        suppressHydrationWarning={true}
+      >
+        {children}
+      </body>
     </html>
   );
 }
